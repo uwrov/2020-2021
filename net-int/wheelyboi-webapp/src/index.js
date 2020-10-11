b110653f690253c551e7226b172f9adce54c316c
Chris <chrisy5@uw.edu>
2020-10-10 17:25:14 -0700

    controller added

--- net-int/wheelyboi-webapp/src/index.js
+++ net-int/wheelyboi-webapp/src/index.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Key from './component/key';
+
+
+ReactDOM.render(
+   <React.StrictMode>
+     <Key />
+   </React.StrictMode>,
+   document.getElementById('root')
+ );
