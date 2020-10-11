e4608bc7ff8ee2ea98f72ac95bc457c25fb5bff5
ungjus <ungjus19@uw.edu>
2020-10-10 17:35:18 -0700

    need to work on in vmware

--- net-int/server/2020_server.py
+++ net-int/server/2020_server.py
@@ -1,6 +1,8 @@
 import json
+import rospy
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
+from geometry_msgs.msg import Twist
 
 HOST_IP = "localhost"
 HOST_PORT = "4040"
@@ -8,14 +10,18 @@ HOST_PORT = "4040"
 app = Flask(__name__)
 sio = SocketIO(app, cors_allowed_origins="*")
 
+#send data as a dicitonary wtih keys: lx, ly,lz, ax, ay, az
+# the values will be the respective values for the Keys
+#send as twist object
 @sio.on("Send Movement")
-def send_movement():
-    emit('Movement', {"speed": speed, "direction": direction}, broadcast=True)
+def send_movement(data):
+    #('Movement', {"speed": speed, "direction": direction}, broadcast=True)
+    msg = Twist()
+    msg.linear.x = data
 
-@sio.on('Send Sensor Data')
 def send_sensor_data():
     emit('Senor Data', {'sensor data': sensor_data}, broadcast=True)
 
 @sio.on('Send Command')
 def send_command(command):
-    emit('Command', command, broadcast=True)
+    #('Command', command, broadcast=True)
