51a06a65b09bbf4fbb09ab06c9d08c9d7b547fec
ungjus <ungjus19@uw.edu>
2020-11-02 18:21:13 -0800

    added documentation to server

--- net-int/server/2020_server.py
+++ net-int/server/2020_server.py
@@ -14,13 +14,30 @@ sio = SocketIO(app, cors_allowed_origins="*")
 velocity_publisher = None
 rate = None
 
+# To run server
 # rosrun wb_sol 2020_server.py
 # source devel/setup.sh
 
-
-#@sio.on("Send Movement")
 @sio.on("Send State")
 def send_movement(state):
+    """
+    Sends contoller input to rospy
+
+    Receives movement information from controller as a JSON object.
+    The movement info is then converted into a Twist object and published
+    to the rospy.
+
+    Parameters
+    -------
+    state : JSON/Dictionary
+        stores the movement of the controller in terms of linear components and
+        anglular components.
+        state = {lin_x: 10, lin_y: 0, lin_z: 0, ang_x: 0, ang_y: 0, ang_z: 3}
+
+    Returns
+    -------
+    None
+    """
     msg = Twist()
     msg.linear.x = state["lin_x"]
     msg.linear.y = state["lin_y"]
@@ -34,22 +51,19 @@ def send_movement(state):
     rate.sleep()
     #rospy.signal_shutdown('task done')
 
-def send_sensor_data():
-    emit('Senor Data', {'sensor data': sensor_data}, broadcast=True)
-
-@sio.on('Send Command')
-def send_command(command):
-    #emit('Command', command, broadcast=True)
-    print(hello)
+# def send_sensor_data():
+#     emit('Senor Data', {'sensor data': sensor_data}, broadcast=True)
+#
+# @sio.on('Send Command')
+# def send_command(command):
+#     #emit('Command', command, broadcast=True)
+#     print(hello)
 
 if __name__ == '__main__':
+    """ Sets up rospy and starts server """
     try:
         rospy.init_node('wheely_boi', anonymous=True)
         velocity_publisher = rospy.Publisher('/wheely_boi/wheely_boi/cmd', Twist, queue_size=10)
         rate = rospy.Rate(10)
         sio.run(app, host=HOST_IP, port=HOST_PORT)
     except rospy.ROSInterruptException: pass
-
-def print_directions(state):
-    for direction in state:
-        print(str(direction) + " = " + str(state[direction]))
