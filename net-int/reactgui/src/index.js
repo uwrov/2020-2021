f922eee06d0bd52bdf0203f66a891c6db4467dbb
9KelvinN <9kelvin@gmail.com>
2020-11-28 18:39:14 -0800

    Moved everything back
    Put everything back to /oldcomponents but updated the references in
    index.js also commented out console

--- net-int/reactgui/src/index.js
+++ net-int/reactgui/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Console from './Console.js';
+import './oldcomponents/index.css';
+import Console from './oldcomponents/Console.js';
 //import App from './App';
-import GUI from "./components/GUI.js";
+import GUI from "./oldcomponents/components/GUI.js";
 
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
 //let button = new Button();
 ReactDOM.render(<GUI />, document.getElementById('root'));
-ReactDOM.render(<Console />, document.getElementById('console'));
+//ReactDOM.render(<Console />, document.getElementById('console'));
 //ReactDOM.render(button.render(), document.getElementById('root'));
