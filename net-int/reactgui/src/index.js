bf580b56b740183665e7735df2452eee529774fb
Chris <chrisy5@uw.edu>
2021-01-07 18:43:08 -0800

    organized for debugging purposes

--- net-int/reactgui/src/index.js
+++ net-int/reactgui/src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom';
 import Console from './components/console/Console.js';
 //import App from './App';
 import GUI from "./oldcomponents/components/GUI.js";
+import Controller from "./components/controller/Controller.js";
 
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
 //let button = new Button();
-ReactDOM.render(<GUI />, document.getElementById('root'));
-//ReactDOM.render(<Console />, document.getElementById('root'));
+//ReactDOM.render(<GUI />, document.getElementById('root'));
+ReactDOM.render(<Console />, document.getElementById('root'));
+//ReactDOM.render(<Controller />, document.getElementById('root'));
 //ReactDOM.render(button.render(), document.getElementById('root'));
