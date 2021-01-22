9a5323f6c022a12dd56087fab09a94aa87c139f6
Andrew <anemnox@gmail.com>
2021-01-21 17:21:09 -0800

    cleaning up some etc files

--- net-int/server/TestClient.py
+++ net-int/server/TestClient.py
@@ -3,14 +3,14 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
 
 HOST_IP = "localhost"
-HOST_PORT = "4040"
+HOST_PORT = "8080"
 
 app = Flask(__name__)
 sio = SocketIO(app, cors_allowed_origins="*")
 
-@sio.on("Test")
+@sio.on("Test Client")
 def get_image(data):
-    sio.emit("Test", broadcast=True)
+    sio.emit("Test Server", broadcast=True)
 
 if __name__ == '__main__':
     sio.run(app, host=HOST_IP, port=HOST_PORT)
