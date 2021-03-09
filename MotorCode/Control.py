675b7b12f48703f9871acd583111c85a24a7c0a1
Daniel Berezansky <23129330+dberezansky@users.noreply.github.com>
2021-03-08 17:05:08 -0800

    Motor Code Clean-up
    Added comments to the code to allow easier use and understanding by other UWROV teams.

--- MotorCode/Control.py
+++ MotorCode/Control.py
@@ -2,16 +2,19 @@ import pigpio
 import curses
 import time
 
+# Placeholder values giving a range for use in roation.
 min = 500
 max = 2500
 slope = (max - min) / 2
 intercept = max - slope * (1)
 
+# Allows the control of motors given an input array and an output array.
 def control(inputArr, output):
-    for item in inputArr: 
+    # Checks if the given input array is valid and shows an error message if not.
+    for item in inputArr:
         if item < -1 or item > 1:
             print("Error")
-    #checking if the fourth value is 0 or not
+    # Case where there is no rotation based on the "R" value of the input array being zero.
     if (inputArr[3] == 0):
         output[4] = (slope * inputArr[2]) + intercept
         output[5] = (slope * inputArr[2]) + intercept
@@ -21,10 +24,13 @@ def control(inputArr, output):
         output[1] = c2
         output[2] = c1
         output[3] = c2
-    else: 
+    # Case where there is rotation based on the "R" value of the input array being non-zero.
+    else:
         c1 = 0
+        # Case where the rotation is clockwise based on a positive "R" value.
         if (inputArr[3] > 0):
             c1 = (slope * inputArr[3]) + intercept
+        # Case where the rotation is counter-clockwise based on a negative "R" value.
         else:
             c1 = (-1) * ((slope * abs(inputArr[3])) + intercept)
         output[0] = c1
@@ -32,15 +38,14 @@ def control(inputArr, output):
         output[2] = c1
         output[3] = c1
 
-    
 
-#Test Code: 
+# Test Code. 
 control([1, 1, 1, 0])
 control([])
 
-#Connection with pi:
+# Connects with the raspberry pi device.
 pi = pigpio.pi()
-
+# Sets the signal pin value on the raspberry pi of each motor.
 pinA = 16
 pinB = 12
 pinC = 26
@@ -50,4 +55,4 @@ pinF = 20
 
 output = [0, 0, 0, 0, 0, 0]
 
-#Process the input
+# Process the control values for each motor.
--- MotorCode/CornerServo.py
+++ MotorCode/CornerServo.py
@@ -2,6 +2,7 @@ import pigpio
 import curses
 import time
 
+# Class to allow operation of a corner servo motor.
 class CornerServo: 
     # Check whether an error occurs for the given input array.
     def __init__(self, x, y, z, r):
@@ -11,13 +12,13 @@ class CornerServo:
             self.z = z
             self.r = r
             self.pi = pigpio.pi()
+        # Checks if the given input array is valid and shows an error message if not.
         except x < -1 or y < -1 or x > 1 or y > 1 or z < -1 or z > 1: 
             print("Error: The given input is invalid.")
         except: 
             print("Error: Non-standard error.")
 
+    # Allows the control of a corner servo motor.
     def controlEF(self):
         self.pi.set_servo_pulsewidth(21, max(500, 1500 - 50))
         time.sleep(0.0001)
-        
-        
