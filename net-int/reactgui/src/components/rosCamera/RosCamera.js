1d97073b99df1cda55c4905354ca0af4a907496f
Chris <chrisy5@uw.edu>
2021-04-07 19:01:50 -0700

    dropdown

--- net-int/reactgui/src/components/rosCamera/RosCamera.js
+++ net-int/reactgui/src/components/rosCamera/RosCamera.js
@@ -6,15 +6,14 @@ export default class Camera extends React.Component {
     super(props);
     this.socket = require("socket.io-client")("http://localhost:4040");
     this.state = {
-      img_src_1:
-        "https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg",
-      img_src_2:
-        "https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg",
-      img_src_3:
-        "https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg"
+      channels: ["https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?fit=641%2C618&ssl=1", "https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg", "https://www.thermofisher.com/blog/food/wp-content/uploads/sites/5/2015/08/single_strawberry__isolated_on_a_white_background.jpg", "https://images-na.ssl-images-amazon.com/images/I/71%2BqAJehpkL._SL1500_.jpg"],
+      curr: 0
     };
 
     console.log(this.socket);
+
+    // get id
+
     this.socket.on("Image Display", (image) => {
       console.log("got image");
       console.log(image.id);
@@ -32,28 +31,37 @@ export default class Camera extends React.Component {
     );
     //const string_char = String.fromCharCode.apply(null, typed_array);
     let imageurl = "data:image/png;base64, " + data;
-    if (image.id == 1) {
-      this.setState({ img_src_1: imageurl })
-    } else if (image.id == 2) {
-      this.setState({ img_src_2: imageurl })
-    } else if (image.id == 3) {
-      this.setState({ img_src_3: imageurl })
-    }
-    // this.setState({ img_src: imageurl });
+    let channels = this.state.channels;
+    channels[image.id] = imageurl;
+    this.setState({ channels: channels});
     var d2 = new Date();
     var m = d2.getTime();
     console.log(m-n)
   };
 
+  renderOptions() {
+    return this.state.channels.map(function(url, i) {
+      return(<option value={i}>{i + 1}</option>);
+    });
+  }
+
   render() {
     return (
-      <div className="camera1">
-        <img src={this.state.img_src_1} alt="Image Display" className="image_1" />
-        <img src={this.state.img_src_2} alt="Image Display" className="image_2" />
-        <img src={this.state.img_src_3} alt="Image Display" className="image_3" />
+      <div className="camera">
+        <p>Channel: </p>
+        <select id="channels" onChange={(event) => {
+          this.setState({
+            curr: event.target.value
+          });
+        }}>
+          {this.renderOptions()}
+        </select>
+        <img src={this.state.channels[this.state.curr]} alt="Image Display" className="image" />
       </div>
     );
   }
+
+  
   // render() {
   //   return (
   //     <div className="camera">
