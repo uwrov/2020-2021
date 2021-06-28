f7df6c20a7def4d044e202683faf9213bc6b0a92
Andrew <anemnox@gmail.com>
2021-06-27 21:39:28 -0700

    Updating UI to react-grid-layout

--- net-int/reactgui/src/datastructs/WidgetLib.js
+++ net-int/reactgui/src/datastructs/WidgetLib.js
@@ -27,4 +27,21 @@ export let WIDGET_DICT = (data) => {
   }
 };
 
+export let getWidgetComponent = (data) => {
+  switch (data.type) {
+    case "settings":
+      return <Settings props={data.savedProps} />;
+    case "ros_camera":
+      return <RosCamera props={data.savedProps} />;
+    case "ip_camera":
+      return <IpCamera props={data.savedProps} />;
+    case "controller":
+      return <Xbox props={data.savedProps} />;
+    case "key_controller":
+      return <Controller props={data.savedProps}/>;
+    case "script_runner":
+      return <ScriptRunner props={data.savedProps}/>;
+  }
+};
+
 export default null;
