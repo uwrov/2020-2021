9a2cc177f961eb0fa0c2c1a37603bbd98fe01d1c
Ben <ben2111@uw.edu>
2021-07-04 19:53:51 -0700

    added code according to api

--- src/nautilus_scripts/scripts/flyover_task/flyover_task_runner.py
+++ src/nautilus_scripts/scripts/flyover_task/flyover_task_runner.py
@@ -6,17 +6,41 @@ ROS driver for the flyover task
 import cv2
 import rospy
 import numpy as np
-import flyover_task.flyover as flyover
-import flyover_task.process_image as process
 from sensor_msgs.msg import CompressedImage
-from std_msgs.msg import String
 
 # variables
-cam = '/nautilus/nautilus/camera1/nautilus_cam/compressed'
-button = '/buttonPress'
+current_frame = None
+instructions = None
+data = []
+
+flying_over = True
+result = None
+
+# paths
+cam1 = '/nautilus/nautilus/camera1/nautilus_cam/compressed'
+output_path = '/home/uwrov/Desktop/flyover_output.png'
 
 def main():
-    return
+    r = rospy.Rate(5)
+    rospy.Subscriber(cam1, CompressedImage, update_frame)
+    while not flying_over:
+        rospy.sleep()
+    result = generate_grid(data)
+    print("saving result")
+    cv2.imwrite(output_path, result)
+
+def update_frame():
+    global data
+    if flying_over:
+        # course adjustment
+        instructions = course_correct(current_frame)
+        adjust_course(instructions)
+        
+        # data collection
+        if frame_contains_row(current_frame):
+            data = snip_row(current_frame)
+
+        # update flying over?
 
 def shutdown_fn():
     print("shutting down")
