ca38d2767a03bd5b40065c56ec255714d3590751
Ben <ben2111@uw.edu>
2021-07-11 20:06:32 -0700

    worked on flyover task

--- src/nautilus_scripts/scripts/flyover_task/flyover_task_runner.py
+++ src/nautilus_scripts/scripts/flyover_task/flyover_task_runner.py
@@ -6,6 +6,8 @@ ROS driver for the flyover task
 import cv2
 import rospy
 import numpy as np
+import threading
+import time
 from sensor_msgs.msg import CompressedImage
 
 # variables
@@ -23,24 +25,53 @@ output_path = '/home/uwrov/Desktop/flyover_output.png'
 def main():
     r = rospy.Rate(5)
     rospy.Subscriber(cam1, CompressedImage, update_frame)
+    course_adjustment = threading.Thread(target=course_adjustment_thread, args=())
+    data_collection = threading.Thread(target=data_collection_thread, args=())
+    course_adjustment.start()
+    data_collection.start()
     while not flying_over:
         rospy.sleep()
+    course_adjustment.join()
+    data_collection.join
     result = generate_grid(data)
     print("saving result")
     cv2.imwrite(output_path, result)
 
-def update_frame():
+def update_frame(msg):
     global data
+    global flying_over
+    global current_frame
+
+    # get current frame
+    np_arr = np.fromstring(msg.data, np.uint8)
+    image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    current_frame = image_np.copy()
+
     if flying_over:
-        # course adjustment
+        if (len(data) == 9):
+            if (len(data[8]) == 3):
+                    if (data[8][2] != None):
+                        flying_over = False
+
+def course_adjustment_thread():
+    global current_frame
+    global flying_over
+    if flying_over:
         instructions = course_correct(current_frame)
         adjust_course(instructions)
-        
-        # data collection
-        if frame_contains_row(current_frame):
-            data = snip_row(current_frame)
+        time.sleep(3)
+    else:
+        return
 
-        # update flying over?
+def data_collection_thread():
+    global current_frame
+    global flying_over
+    if flying_over:
+        if frame_contains_row(current_frame):
+            data.append(snip_row(current_frame))
+        time.sleep(3)
+    else:
+        return
 
 def shutdown_fn():
     print("shutting down")
