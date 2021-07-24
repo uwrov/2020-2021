0d28ec105b4319eb5f3955fe520c12ef0e3a05d9
Atharva Kashyap <a380ask@outlook.com>
2021-07-24 23:42:28 +0100

    Added some scalars for the motors

--- src/nautilus_motors/src/MotorCode/Control Copy 7-3-21.py
+++ src/nautilus_motors/src/MotorCode/Control Copy 7-3-21.py
@@ -49,45 +49,10 @@ def control(controlInputs):
     controlInputs[1] *= 1     # Y
     controlInputs[2] *= -1    # Z
     controlInputs[3] *= 1     # R
-
-    # Case where there is no rotation based on the "R" value of the input array being zero.
-    # if (controlInputs[3] == 0):
-    #     # calculate motor constants
-    #     Ac = 0.5 * (controlInputs[0] - controlInputs[1])
-    #     Bc = 0.5 * (controlInputs[0] + controlInputs[1])
-    #     Cc = 0.5 * (controlInputs[0] + controlInputs[1])
-    #     Dc = 0.5 * (controlInputs[0] - controlInputs[1])
-    #     Ec = controlInputs[2]
-    #     Fc = Ec
-        
-    #     # apply constants onto motors
-    #     apply_motor(controlOutputs, 0, Ac) 
-    #     apply_motor(controlOutputs, 1, Bc) 
-    #     apply_motor(controlOutputs, 2, Cc) 
-    #     apply_motor(controlOutputs, 3, Dc) 
-    #     apply_motor(controlOutputs, 4, Ec) 
-    #     apply_motor(controlOutputs, 5, Fc) 
-    # else: 
-    # # rotation is clockwise based on a positive "R" value.
-    #     c1 = controlInputs[3] / 2
-    #     c2 = -1 * c1
-        
-    #     apply_motor(controlOutputs, 0, c2)
-    #     apply_motor(controlOutputs, 1, c1)
-    #     apply_motor(controlOutputs, 2, c2)
-    #     apply_motor(controlOutputs, 3, c1)
-
-    #     # zero out vertical motors
-    #     apply_motor(controlOutputs, 4, 0)
-    #     apply_motor(controlOutputs, 5, 0)
-    # return controlOutputs
     
     c1 = controlInputs[3] / 2
     # c2 = -1 * c1
 
-    # NEXT TIME WE WORK: If the input values are less than 0, then we would have to add the c1.
-    # If we have input values that are more than 0, then we would need to subtract the c1. 
-    # making this work will be your next task!
     # Test Cases: [1, 0.5, 0.25, 0.5, 0, 0]; [1, 0.5, 0.5, 0.25, 1, 1]; [1, 0, -0.5, 0.25, 0, 1]
 
     # ----------------------------------------------------------
@@ -95,15 +60,15 @@ def control(controlInputs):
     # 2. Apply R (direction) as scaled be speed (magnitude)
 
     max_r_speed = 2500
-    min_r_speed = 1500
+    min_r_speed = 500
     max_l_speed = 2500
-    min_l_speed = 1500
+    min_l_speed = 500
     r = controlInputs[3]
     
     # TO DO: Limit on Maximum and Minimum Linear Speed
     # Determine rotational and linear, maximum and minimum, speeds.
-    limit_max = (max_r_speed + 2 * (max_l_speed)) / 3
-    limit_min = (min_r_speed + 2 * (min_l_speed)) / 3
+    limit_max = (max_r_speed + (2 * max_l_speed)) / 3000
+    limit_min = (min_r_speed + (2 * min_l_speed)) / 3000
 
     # TO DO: R mapped (-1, 0, 1)
     # Determine rotational value source.
@@ -142,8 +107,9 @@ def apply_control(_input, pi):
 if __name__ == '__main__':
     pi = pigpio.pi()
     
-    outputs = [1500, 1500, 1500, 1500, 1500, 1500]
-    #outputs = control(inputs)
+    inputs = [1, 0.5, 0.25, 0.5, 0, 0]
+    outputs = control(inputs)
+    print(outputs)
     # Operates the servo connected to each pin at a specific power.
     pi.set_servo_pulsewidth(pin1, outputs[0])
     pi.set_servo_pulsewidth(pin2, outputs[1])
@@ -151,7 +117,6 @@ if __name__ == '__main__':
     pi.set_servo_pulsewidth(pin4, outputs[3])
     pi.set_servo_pulsewidth(pin5, outputs[4])
     pi.set_servo_pulsewidth(pin6, outputs[5])
-    print(outputs)
     time.sleep(20)
 
     pi.stop()
